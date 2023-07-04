94f92178d88ae33a9662b1e97758eb2f03d6aac2
prettymlouis <prettymlouis@gmail.com>
2023-07-04 14:37:51 +0700

    Update streamlit_app.py
    add

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit
+import pandas
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🍳 Omega 3 scrambled egg')
 streamlit.text('🥗 Kale and goat cheese salad')
 streamlit.text('🥑 Avocado toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
