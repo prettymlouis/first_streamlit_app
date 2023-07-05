5c684f997ad5a9d4f9bf64118b2878aad3c47e8a
prettymlouis <prettymlouis@gmail.com>
2023-07-05 18:17:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,6 @@ import snowflake.connector
 from urllib.error import URLError
 
 
-
-
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
